432ea1f12d90487f05b63ca61562b8e0e3aa2096
fojletuhin <fojletuhin@gmail.com>
2024-03-16 15:18:41 +0600

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,24 @@ function App() {
     <>
       
       <div className='w-4/5 m-auto'>
-      
+        <nav>
+          <div>
+            Chefs-Table
+          </div>
+          <div>
+            <ul>
+              <li>Home</li>
+              <li>Recipe</li>
+              <li>About</li>
+              <li>Search</li>
+            </ul>
+          </div>
+          <div>
+          <input type="text" placeholder="Search" className="input input-bordered input-accent w-full max-w-xs" /></div>
+          <img src="./assets/profile.png" alt="" />
+          <img src="./assets/Rectangle 1.png" alt="" />
+        </nav>
+
       </div>
       
     </>
